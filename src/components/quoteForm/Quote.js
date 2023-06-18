bbef361dca428a04a18a5267c9298de5bc569e65
Ricardo Maya <rickmaya@gmail.com>
2023-06-18 00:08:39 -0500

    add styling to the form container, form check button, and the form button

--- src/components/quoteForm/Quote.js
+++ src/components/quoteForm/Quote.js
@@ -1,15 +1,25 @@
 import React from "react";
+import './Quote.css';
 
 const Quote = () => {
 
   return (
-    <>
-      {/*TODO:make a border for the form*/}
+    <div id="quoteForm-container">
       <div className="mb-3">
-        <input type="text" className="form-control" id="name" placeholder="Name"/>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          placeholder="Name"
+        />
       </div>
       <div className="mb-3">
-        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email"/>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleFormControlInput1"
+          placeholder="Email"
+        />
       </div>
       <div className="mb-3">
         <select className="form-select" id="dropdownField" data-placeholder="Select a capability">
@@ -20,17 +30,35 @@ const Quote = () => {
         </select>
       </div>
       <div className="mb-3">
-        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Comments"></textarea>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+          placeholder="Comments">
+        </textarea>
       </div>
       <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-          <label className="form-check-label" htmlFor="flexCheckDefault">
-            I'd like to receive the newsletter
-          </label>
+        <input
+          className="form-check-input"
+          type="checkbox"
+          value=""
+          id="flexCheckDefault"
+        />
+        <label
+          className="form-check-label"
+          htmlFor="flexCheckDefault"
+        >
+          I'd like to receive the newsletter
+        </label>
       </div>
       {/* TODO: change color of button text*/}
-      <button type="button" className="btn btn-info">Get a quote</button>
-    </>
+      <button
+        type="button"
+        className="btn btn-info custom-button"
+      >
+        Get a quote
+      </button>
+    </div>
   )
 }
 
