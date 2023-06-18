cb3320746c5ca682481c95f2cd719c181f0bd513
Ricardo Maya <rickmaya@gmail.com>
2023-06-18 14:17:11 -0500

    wrap headings and article in a div and add flex display and flex-flow css properties for styling

--- src/components/homePage/homePageBody/HomeBody.js
+++ src/components/homePage/homePageBody/HomeBody.js
@@ -1,17 +1,22 @@
 import React from "react";
 import './HomeBody.css';
 import Quote from "../../quoteForm/Quote";
+
 const HomeBody = () => {
-  return(
+  return (
     <div id="home-section-wrapper">
-      <div id="heading-container">
+      <div id="heading-article-container">
         <h1 id="heading">Design. Manufacture. Deliver</h1>
-      </div>
-      <div id="article">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div id="article">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.</p>
+        </div>
       </div>
       <div className="quoteForm-homebody-wrapper">
-        <Quote />
+        <Quote/>
       </div>
     </div>
   )
