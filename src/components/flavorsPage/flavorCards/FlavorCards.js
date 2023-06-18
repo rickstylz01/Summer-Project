5b1e86ce2f64e4e77d181fd63241976541be7963
Ricardo Maya <rickmaya@gmail.com>
2023-06-18 18:41:38 -0500

    add a new div to contain the flavors cards header and cards to distribute accordingly

--- src/components/flavorsPage/flavorCards/FlavorCards.js
+++ src/components/flavorsPage/flavorCards/FlavorCards.js
@@ -5,66 +5,67 @@ import Quote from "../../quoteForm/Quote";
 const FlavorCards = () => {
   return (
     <div id="flavorsCards-body-wrapper">
-      <div className="flavor-header">
-        <h1>Flavors</h1>
-      </div>
-      <div id="cards-wrapper" className="row row-cols-1 row-cols-md-2 g-4">
-        {/*TODO: get rid of the underline for the card titles*/}
-        <div className="col">
-          <a href="#">
-            <div className="card">
-              <img
-                src={process.env.PUBLIC_URL + '/assets/images/soft-fruits.jpg'}
-                className="card-img-top"
-                alt="a spread of soft fruits"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Soft Fruits</h5>
-              </div>
-            </div>
-          </a>
+      <div id="header-cards-container">
+        <div className="flavor-header">
+          <h1>Flavors</h1>
         </div>
-        <div className="col">
-          <a href="#">
-            <div className="card">
-              <img
-                src={process.env.PUBLIC_URL + '/assets/images/citrus.jpg'}
-                className="card-img-top"
-                alt="citrus fruits slices lined up from small to big"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Citrus</h5>
+        <div id="cards-wrapper" className="row row-cols-1 row-cols-md-2 g-4">
+          <div className="col">
+            <a href="#">
+              <div className="card">
+                <img
+                  src={process.env.PUBLIC_URL + '/assets/images/soft-fruits.jpg'}
+                  className="card-img-top"
+                  alt="a spread of soft fruits"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Soft Fruits</h5>
+                </div>
               </div>
-            </div>
-          </a>
-        </div>
-        <div className="col">
-          <a href="#">
-            <div className="card">
-              <img
-                src={process.env.PUBLIC_URL + '/assets/images/tropical.jpg'}
-                className="card-img-top"
-                alt="a dragon fruit sliced in half"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Tropical</h5>
+            </a>
+          </div>
+          <div className="col">
+            <a href="#">
+              <div className="card">
+                <img
+                  src={process.env.PUBLIC_URL + '/assets/images/citrus.jpg'}
+                  className="card-img-top"
+                  alt="citrus fruits slices lined up from small to big"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Citrus</h5>
+                </div>
               </div>
-            </div>
-          </a>
-        </div>
-        <div className="col">
-          <a href="#">
-            <div className="card">
-              <img
-                src={process.env.PUBLIC_URL + '/assets/images/fruits-etc.jpg'}
-                className="card-img-top"
-                alt="a man in front of a fruit market"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Etc...</h5>
+            </a>
+          </div>
+          <div className="col">
+            <a href="#">
+              <div className="card">
+                <img
+                  src={process.env.PUBLIC_URL + '/assets/images/tropical.jpg'}
+                  className="card-img-top"
+                  alt="a dragon fruit sliced in half"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Tropical</h5>
+                </div>
               </div>
-            </div>
-          </a>
+            </a>
+          </div>
+          <div className="col">
+            <a href="#">
+              <div className="card">
+                <img
+                  src={process.env.PUBLIC_URL + '/assets/images/fruits-etc.jpg'}
+                  className="card-img-top"
+                  alt="a man in front of a fruit market"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Etc...</h5>
+                </div>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
       <Quote/>
