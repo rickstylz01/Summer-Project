1562d68f02f1c36793b3d21106690da7d1f35bea
Ricardo Maya <rickmaya@gmail.com>
2023-06-17 22:17:43 -0500

    add a display flex property to the header css and justify-content property set to start also move Navbar component to its own directory

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './Header.css';
-import Navbar from "./Navbar";
+import Navbar from "../navbar/Navbar";
 const Header = () => {
  return(
    <div id="header-container">
