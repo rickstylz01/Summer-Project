cacf3e220c5561da3938b5e03265bda5d09db5ef
Ricardo Maya <rickmaya@gmail.com>
2023-06-18 21:27:06 -0500

    wrapped each element in flavorCategories component in its own div

--- src/components/flavorsPage/flavorCategories/FlavorCategories.js
+++ src/components/flavorsPage/flavorCategories/FlavorCategories.js
@@ -1,20 +1,27 @@
 import React from "react";
+import './FlavorCategories.css'
 import Quote from "../../quoteForm/Quote";
 
 const FlavorCategories = () => {
   return (
-    <div id="">
-      <h1>Flavor category name</h1>
+    <div id="flavor-categories-wrapper">
+      <div id="flavor-categories-header">
+        <h1>Flavor category name</h1>
+      </div>
       {/*TODO: align text to the right*/}
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">Aldehyde-Free, Berry</li>
-        <li className="list-group-item">Aldehyde-Free, Cherry</li>
-        <li className="list-group-item">Apple (various)</li>
-        <li className="list-group-item">Apple, Baked</li>
-        <li className="list-group-item">Apricot</li>
-        <li className="list-group-item">Etc...</li>
-      </ul>
-      <Quote />
+      <div id="list-group-container">
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Aldehyde-Free, Berry</li>
+          <li className="list-group-item">Aldehyde-Free, Cherry</li>
+          <li className="list-group-item">Apple (various)</li>
+          <li className="list-group-item">Apple, Baked</li>
+          <li className="list-group-item">Apricot</li>
+          <li className="list-group-item">Etc...</li>
+        </ul>
+      </div>
+      <div id="flavorCategories-quoteForm-container">
+        <Quote/>
+      </div>
     </div>
   )
 }
