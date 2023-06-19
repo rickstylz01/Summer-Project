4680969ee009bfc26ae58a08d21307e5b1542940
Ricardo Maya <rickmaya@gmail.com>
2023-06-19 12:50:59 -0500

    make logo in Header component a link to the home page

--- src/components/header/navbar/Navbar.js
+++ src/components/header/navbar/Navbar.js
@@ -1,18 +1,29 @@
 import React from "react";
 
 const Navbar = () => {
-  return(
+  return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle px-5" aria-current="page" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className="nav-link dropdown-toggle px-5"
+                 aria-current="page"
+                 href="#"
+                 role="button"
+                 data-bs-toggle="dropdown"
+                 aria-expanded="false"
+              >
                 Capabilities
               </a>
               <ul className="dropdown-menu">
